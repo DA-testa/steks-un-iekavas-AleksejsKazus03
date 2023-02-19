61418e1a2acee06b7ad82bf9a983812fb38f89b2
AleksejsKazus03 <125798333+AleksejsKazus03@users.noreply.github.com>
2023-02-19 15:10:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
+        
+        if next in "I":
+            continue
+            
+            
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i+1))
